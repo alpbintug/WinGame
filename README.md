51b50d99b5f69aa256b43232e9ee8f6f2a12cbc0
alpbintug <alp.bintug@gmail.com>
2021-08-24 15:03:38 +0300

    Drawing rectangles and moving them around

--- README.md
+++ README.md
@@ -2,8 +2,27 @@
 
 WinGame is the repository where I try to code a game without any game engine and with plain C. Yeah, I love pain.
 
+## TO-DO
+- ~~Add Paint Pixel Function (DONE)~~
+- ~~Add Fill Rectangle Function (DONE)~~
+- ~~Add Character Movement (DONE: Left, Right, Up, Down)~~
+- Add Draw Rectangle Function (With specified THICCCness)
+- Add Fill Circle Function (Maybe just do a ellipse function and call it inside Circle function with heigh=width?)
+- Add Draw Circle Function (With specified THICCCCCCness)
+- Add Draw Triangle Function
+- Add Draw Line Function
+- Add Draw Rotated Rectangles Function
+- Add Character Rotation
+- Add Time-based functions
+- Add Projectiles
+- Add Enemies
+- Add Scoring system
+- Add Mouse Pointing?
+- Seperate Functions Into Another File
+
 ## How to run?
 
+This project uses windows.h, therefore you need a Windows device to run it. (linux fans plz don't murder me in cold blood)
 gdi32.lib is necessary
 
 #### Compilation
--- main.c
+++ main.c
@@ -3,20 +3,68 @@
 
 #include <windows.h>
 #include <stdint.h>
+#include <stdio.h>
 
-#define Pixel uint32_t
+//4 byte pointer to memory
+typedef uint32_t Pixel;
+//4 byte color value
+typedef uint32_t Color32;
 
+typedef struct Point2D{
+    int X;
+    int Y;
+} Point2D;
+
+Point2D player;
 int running = 1;
-int width = 0;
-int height = 0;
+int clientWidth = 0;
+int clientHeight = 0;
 void* memory;
 BITMAPINFO bitmapInfo;
 
-int PaintPixel(int X, int Y, Pixel color){
+int FillScreen(Color32 color){
+    Pixel *pixel = (Pixel *)memory;
+    int i;
+
+    for(i=0;i<clientWidth*clientHeight;i++){
+        *pixel++=color;
+    }
+}
+int IsPointInBoundaries(Point2D point2d){
+    if(point2d.X>=clientWidth||point2d.Y>=clientHeight||point2d.X<0||point2d.Y<0){
+        printf("Target point out of bounds. Height: %d, Y: %d, Width: %d, X: %d",clientHeight,point2d.Y,clientWidth,point2d.X);
+        return 0;
+    }
+    return 1;
+}
+
+int PaintPixel(Point2D point2d, Color32 color){
+
+    //Consider change this to modulo operator for wrapping around?
+    if(!IsPointInBoundaries(point2d))
+        return -1;
     Pixel* pixel = (Pixel *)memory;
     
-    pixel+=+ Y*width + X;
+    pixel+= point2d.Y*clientWidth + point2d.X;
     *pixel = color;
+    return 0;
+}
+
+
+int FillRectangle(Point2D point2d, int rectangleWidth, int rectangleHeight, Color32 color){
+    if(!IsPointInBoundaries(point2d))
+        return -1;
+    Pixel *pixel = (Pixel *)memory;
+
+    pixel+= point2d.Y*clientWidth + point2d.X;
+
+    int i,j;
+    for(i=0;i<rectangleWidth&&i<clientWidth;i++){
+        for(j=0;j<rectangleHeight&&j<clientHeight;j++){
+            *pixel++=color;
+        }
+        pixel+=clientWidth - rectangleWidth;
+    }
 }
 
 LRESULT CALLBACK WindowProc(HWND handleWindow, UINT message, WPARAM wParam, LPARAM lParam){
@@ -32,6 +80,29 @@ LRESULT CALLBACK WindowProc(HWND handleWindow, UINT message, WPARAM wParam, LPAR
             running = 0;
             return 0;
         }break;
+        case WM_KEYDOWN:
+        {
+            switch(wParam)
+            {
+                case VK_RIGHT:
+                {
+                    player.X++;
+                }break;
+                case VK_LEFT:
+                {
+                    player.X--;
+                }break;
+                case VK_UP:
+                {
+                    player.Y++;
+                }break;
+                case VK_DOWN:
+                {
+                    player.Y--;
+                }
+
+            }
+        }
         default:
         {
             return DefWindowProc(handleWindow,message,wParam,lParam);
@@ -43,7 +114,7 @@ LRESULT CALLBACK WindowProc(HWND handleWindow, UINT message, WPARAM wParam, LPAR
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     PSTR lpCmdLine, INT nCmdShow)
 {
-    #pragma region VARIABLES
+    #pragma region CREATING WINDOW
     WNDCLASS windowClass = {0};
 
     const wchar_t CLASS_NAME[] = L"WinGame";
@@ -74,20 +145,22 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 #pragma endregion
     RECT rect;
     GetClientRect(handleWindow, &rect);
-    width = rect.right-rect.left;
-    height = rect.bottom-rect.top;
+    clientWidth = rect.right-rect.left;
+    clientHeight = rect.bottom-rect.top;
     //Each pixel is 32 bits, therefore 4 bytes, thats why we multiply width and height by 4
-    memory = VirtualAlloc(0, width*height*4, MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE);
+    memory = VirtualAlloc(0, clientWidth*clientHeight*4, MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE);
 
-
-    PaintPixel(10,0,0xFFFFFF);
-
+    Point2D point;
+    point.X = 33;
+    point.Y = 33;
+    player.X = 0;
+    player.Y = 0;
 
     bitmapInfo.bmiHeader;
 
     bitmapInfo.bmiHeader.biSize =sizeof(bitmapInfo.bmiHeader);
-    bitmapInfo.bmiHeader.biWidth = width;
-    bitmapInfo.bmiHeader.biHeight = height;
+    bitmapInfo.bmiHeader.biWidth = clientWidth;
+    bitmapInfo.bmiHeader.biHeight = clientHeight;
     bitmapInfo.bmiHeader.biPlanes = 1;
     bitmapInfo.bmiHeader.biBitCount = 32;
     bitmapInfo.bmiHeader.biCompression = BI_RGB;
@@ -106,11 +179,16 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
             }
             TranslateMessage(&message);
             DispatchMessage(&message);
+
+            FillScreen(0x000000);
+            FillRectangle(player,100,100,0xFF0000);
+
+
             int resultStretchDIBits = StretchDIBits(deviceContext,
                 0,0,
-                width, height,
+                clientWidth, clientHeight,
                 0,0,
-                width,height,
+                clientWidth,clientHeight,
                 memory,
                 &bitmapInfo,
                 DIB_RGB_COLORS,
@@ -118,4 +196,5 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                 );
         }
     }
+    return 0;
 }
